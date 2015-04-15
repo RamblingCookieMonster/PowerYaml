a3930e3c39390598f48aae9518c0e0c18daea36c
Doug Finke <finked@hotmail.com>
2015-04-15 19:07:43 -0400

    Update

--- Initial.Tests.ps1
+++ Initial.Tests.ps1
@@ -87,3 +87,66 @@ languages:
 
     }
 }
+
+Describe "a sequence" {
+    $s = @"
+foods:
+    - Apple
+    - Orange
+    - Strawberry
+    - Mango
+"@
+
+    $result = $s | ConvertFrom-Yaml
+    
+    Context "This sequence" {
+        It "should be an array" {
+            $result.foods.gettype().basetype.name | Should Be Array
+        }
+
+        It "should have 4 items" {
+            $result.foods.count | Should Be 4
+        }
+
+        It "should have an Apple" {
+            $result.foods[0] | Should Be Apple
+        }
+
+        It "should have an Orange" {
+            $result.foods[1] | Should Be Orange
+        }
+
+        It "should have an Strawberry" {
+            $result.foods[2] | Should Be Strawberry
+        }
+
+        It "should have an Mango" {
+            $result.foods[3] | Should Be Mango
+        }
+    }
+}
+
+Describe "a map" {
+    $s = @"
+languages:
+    ruby: Elite
+    python: Elite
+    dotnet: Lame
+"@
+
+    $result = $s | ConvertFrom-Yaml
+    
+    Context "This map" {
+        It "should be an object" {
+            $result.languages.gettype().basetype.name | Should Be Object
+        }
+
+        It "should have a property ruby with a value elite" {
+            $result.languages.ruby | Should Be Elite
+        }
+
+        It "should have a property python with a value elite" {
+            $result.languages.python | Should Be Elite
+        }
+    }
+}
